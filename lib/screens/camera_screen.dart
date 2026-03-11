9add7add17529f9615858d2b706a1d7f2abbd375
MarkTime Pro <marktime@pro.com>
2026-03-11 10:03:20 -0400

    chore: bump version to 1.0.20 for OTA update

--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -499,7 +499,7 @@ class _CameraScreenState extends State<CameraScreen> {
       final String config = const JsonEncoder.withIndent('  ').convert({
         'settings': _settings,
         'customTitle': _customTitle,
-        'version': '1.0.19',
+        'version': '1.0.20',
         'exportedAt': DateTime.now().toIso8601String(),
       });
       final tempDir = await getTemporaryDirectory();
@@ -638,7 +638,7 @@ class _CameraScreenState extends State<CameraScreen> {
                               borderRadius: BorderRadius.circular(20),
                             ),
                             child: Text(
-                              'v1.0.19',
+                              'v1.0.20',
                               style: GoogleFonts.outfit(
                                 color: Colors.black,
                                 fontSize: 11,
